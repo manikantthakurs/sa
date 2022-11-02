8c7538aca0a63fe9f34d3d3ba7ae989acc0e93f7
MANIKANT THAKUR <98531214+manikantthakurs@users.noreply.github.com>
2022-11-02 08:15:24 +0530

    Add files via upload

---
+++ middleware/src/middleware/Client.java
@@ -0,0 +1,9 @@
+package middleware;
+public class Client {
+public static void main(String [] args)
+{
+	InterfaceCalculator i=new Server();
+System.out.println(i.add(12,13));
+System.out.println(i.sub(12,12));
+}
+}
---
+++ middleware/src/middleware/InterfaceCalculator.java
@@ -0,0 +1,5 @@
+package middleware;
+public interface InterfaceCalculator{
+public int add(int a,int b);
+public int sub(int a,int b);
+}
